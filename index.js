bc145031ebddb3ff1eef9fdfa5c01fe209359d78
arsalan aslam <arsalanaslam475@gmail.com>
2024-02-17 22:26:20 +0500

    changing port

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ app.get("/login", (req, res) => {
   res.send("<h1>Login</h1>");
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   console.log("server is Started ", process.env.PORT);
 });
