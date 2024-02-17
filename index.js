f88a0f50cce81db3fe637f1fe50ec6983f2110c2
arsalan aslam <arsalanaslam475@gmail.com>
2024-02-17 22:23:17 +0500

    no error

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-require("dotenv").config();
+const env = require("dotenv");
+env.config();
+console.log(process.env.PORT);
 
 const express = require("express");
 
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send(`Home`);
+  res.send("<h1>Home Page</h1>");
 });
 
 app.get("/login", (req, res) => {
-  res.send("Login");
+  res.send("<h1>Login</h1>");
 });
 
 app.listen(3000, () => {
-  console.log("server is Started ");
+  console.log("server is Started ", process.env.PORT);
 });
